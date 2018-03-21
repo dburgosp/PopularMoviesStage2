8b65414aef2f47fe564f1ab48b3c02ec2700046a
David Burgos Prieto <david.burgos.prieto@gmail.com>
2018-03-21 17:55:05 +0100

    Mockups for main activity

---
+++ app/src/main/java/com/example/android/popularmoviesstage2/activities/prueba.java
@@ -0,0 +1,15 @@
+package com.example.android.popularmoviesstage2.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.popularmoviesstage2.R;
+
+public class prueba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aaa_prueba);
+    }
+}
